1a299e789199777817ad5a8705e1640c1494a8ff
João Matias <jmatiasn@gmail.com>
2021-03-06 21:52:15 -0300

    Adicionado script de criação do BD

---
+++ back-end/SQL Criação do BD.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE RRTCEDB;
+
+CREATE TABLE Usuario (
+id int IDENTITY(1,1) NOT NULL,
+username VARCHAR(60) NOT NULL,
+usertype VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL,
+passwd VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE TCC (
+id INT IDENTITY(1,1) NOT NULL,
+title VARCHAR(60) NOT NULL,
+author_id INT NOT NULL,
+professor_id INT NOT NULL,
+approved BIT default 'FALSE',
+keywords VARCHAR(160),
+abstract VARCHAR(500),
+date_creation DATE ,
+PRIMARY KEY (id),
+FOREIGN KEY (author_id) REFERENCES Usuario(id),
+FOREIGN KEY (professor_id) REFERENCES Usuario(id),
+);
